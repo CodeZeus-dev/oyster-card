b1d2cec9d341dc0baae62e28584a32dc2ec8e548
CodeZeus-dev <constantine.akritides@gmail.com>
2020-11-02 15:32:09 +0000

    added top_up method, and the unit tests that check for reponsiveness, and whether funds can be added into the balance via top_up

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,2 +1,15 @@
 class Oystercard
+
+  attr_reader :balance
+
+  $DEFAULT_BALANCE = 0
+
+  def initialize(balance = $DEFAULT_BALANCE)
+    @balance = balance
+  end
+
+  def top_up(value)
+    @balance += value
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,23 @@
 require 'oystercard'
 
 describe Oystercard do
-  
+  it "returns an instance of Oystercard class" do
+    expect(subject).to be_instance_of(Oystercard)
+  end
+
+  describe "#initialize" do
+    it "initializes the card with 0 balance in it" do
+      expect(subject.balance).to eq(0)
+    end
+  end
+
+  describe "#top_up" do
+    it "can be called on an Oystercard instance" do
+      expect(subject).to respond_to(:top_up).with(1).argument
+    end
+
+    it "adds funds to the card's balance" do
+      expect { subject.top_up(1) }.to change { subject.balance }.from(0).to(1)
+    end
+  end
 end
