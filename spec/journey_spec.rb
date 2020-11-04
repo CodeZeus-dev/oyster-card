869d0cdb038a9c03635556a0fa8ade6c5cb54f7a
CodeZeus-dev <constantine.akritides@gmail.com>
2020-11-04 20:33:39 +0000

    added initialize method in Journey class that creates two instance vairables (entry_station, exit_station), also added the corresponding unit tests

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -6,4 +6,14 @@ describe Journey do
     expect(subject).to be_instance_of(described_class)
   end
 
+  describe "#initialize" do
+    it "creates an empty_station instance variable upon initialisation" do
+      expect(subject.entry_station).to eq nil
+    end
+
+    it "creates an exit_station instance variable upon initialisation" do
+      expect(subject.exit_station).to eq nil
+    end
+  end
+
 end
